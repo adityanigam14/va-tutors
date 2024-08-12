d37af2aa1d8b75eaea725bb3f267daa9f13ec4df
Aashish Anantharaman <68557624+ABlizzard07@users.noreply.github.com>
2024-08-12 14:41:14 -0400

    hide logo

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,51 +1,14 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import logo from './imagery/misc/logo.png';
-import { HiMenu, HiX } from 'react-icons/hi'; 
 
 export default function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
-    <nav className="flex flex-col sm:flex-row justify-between items-center p-5 bg-blue-400 w-screen h-20 fixed z-50">
-
-      <Link to="/" className="flex items-center text-white text-xl mb-4 sm:mb-0">
-        <img src={logo} alt="Logo" className="mr-2 mb-2 h-16 w-16" /> 
+    <nav className="flex justify-between items-center p-5 bg-blue-400 w-screen h-20 fixed z-50">
+      <Link to="/" className="flex items-center text-white text-xl mr-5">
+        <img src={logo} alt="Logo" className="h-16 w-16 hidden sm:block" /> 
         <span className="hidden sm:block text-xl">Home</span>
       </Link>
-
-      <div className="sm:hidden flex items-center">
-        <button onClick={() => setIsOpen(!isOpen)} className="text-white text-3xl">
-          {isOpen ? <HiX /> : <HiMenu />}
-        </button>
-      </div>
-
-      <div className={`fixed inset-0 bg-blue-400 transition-transform transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} z-50`}>
-        <ul className="flex flex-col items-center space-y-4 p-10">
-          <li>
-            <Link to="/services" className="text-white text-xl" onClick={() => setIsOpen(false)}>
-              Services
-            </Link>
-          </li>
-          <li>
-            <Link to="/team" className="text-white text-xl" onClick={() => setIsOpen(false)}>
-              Our Team
-            </Link>
-          </li>
-          <li>
-            <Link to="/partners" className="text-white text-xl" onClick={() => setIsOpen(false)}>
-              Partners
-            </Link>
-          </li>
-          <li>
-            <Link to="/feedback" className="text-white text-xl" onClick={() => setIsOpen(false)}>
-              Feedback
-            </Link>
-          </li>
-        </ul>
-      </div>
-
-      <ul className={`flex space-x-4 ${isOpen ? 'hidden' : 'block'} sm:block`}>
+      <ul className="flex space-x-4">
         <li>
           <Link to="/services" className="text-white text-xl">
             Services
