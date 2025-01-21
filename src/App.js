b1ef162b8636733cab8ef9cc0e6f61b19da9e260
Aashish Anantharaman <aashu.ananth@gmail.com>
2025-01-21 12:16:58 -0500

    Revert "in person tutoring too"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import Team from "./pages/Team";
+import Partners from "./pages/Partners";
+import Feedback from "./pages/Feedback";
+import Classroom from "./pages/Classroom";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'auto' });
+  }, [location.pathname]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col items-center">
+      <Navbar />
+      <div className="pt-16 w-screen">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/partners" element={<Partners />} />
+          <Route path="/feedback" element={<Feedback />} />
+          <Route path="/classroom" element={<Classroom />} />
+        </Routes>
+      </div>
+      <Footer />
     </div>
   );
 }
